83bd8fedc082b9f6b140067878e2ea5d9758cf4c
Garrett Reynolds <wgreynolds@crimson.ua.edu>
2022-02-27 19:07:51 -0600

    setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mis321_pa2_garrettwreynolds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
